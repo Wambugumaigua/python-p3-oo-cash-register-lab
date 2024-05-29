602c6b885117fd8c7e84e1cb62816ce3d36c0042
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-05-29 08:03:27 -0700

    complete lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.items = []
+        self.last_transaction_amount = 0
+        self.discount = discount
+
+
+    def add_item(self, item_cost):
+        self.total += item_cost
+        self.items.append(item_cost)
+        self.last_transaction_amount = item_cost
+
